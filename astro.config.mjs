d037e84abea86ef06a23cc4f820d8c2a27718b5b
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-02-17 20:57:37 +0200

    init commit, Ready for HACK AI

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  output: "server",
+  adapter: cloudflare()
+});
