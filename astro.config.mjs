3c9acb03710e278fd94a792f338fe7be9f19e7f5
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-02-20 19:59:23 +0200

    adding vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
